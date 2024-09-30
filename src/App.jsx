b61240f1fff75590a2611e5efc53f2a4700bf499
George <george.alexander.saav@gmail.com>
2024-09-30 09:15:16 +0200

    Workshop done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { appsData, productsData } from "./data.js";
 import "./App.css";
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import ProductsPage from "./pages/Products/index.jsx";
+import Home from "./pages/Home/index.jsx";
+import AppsPage from './pages/Apps/index.jsx'
+import { Link } from "react-router-dom";
 
 function App() {
   const [products] = useState(productsData)
@@ -14,12 +19,20 @@ function App() {
         <h1>Apple Shop</h1>
         <nav>
           <ul>
-            <li>Home</li>
-            <li>Products</li>
-            <li>Apps</li>
+            <li>
+              <Link to="/">Home</Link></li>
+            <li>
+              <Link to="/products">Products</Link></li>
+            <li>
+              <Link to="/apps">Apps</Link></li>
           </ul>
         </nav>
       </header>
+      <Routes>
+        <Route path ="/" element={<Home/>}/>
+        <Route path ="/products" element={<ProductsPage products={products}/>}/>
+        <Route path ="/apps" element={<AppsPage/>}/>
+      </Routes>
     </div>
   );
 }
